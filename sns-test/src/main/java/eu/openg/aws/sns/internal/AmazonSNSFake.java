efa7732f45d430ca7a27c7f1d19ad6526e84648b
Kęstutis Triponis <kestutis@openg.lt>
2015-10-07 19:03:58 +0300

    SNS fake cleanup

--- sns-test/src/main/java/eu/openg/aws/sns/internal/AmazonSNSFake.java
+++ sns-test/src/main/java/eu/openg/aws/sns/internal/AmazonSNSFake.java
@@ -17,8 +17,11 @@
 package eu.openg.aws.sns.internal;
 
 import com.amazonaws.AmazonClientException;
-import com.amazonaws.services.sns.model.CreateTopicResult;
-import com.amazonaws.services.sns.model.PublishResult;
+import com.amazonaws.AmazonWebServiceRequest;
+import com.amazonaws.ResponseMetadata;
+import com.amazonaws.regions.Region;
+import com.amazonaws.services.sns.AmazonSNS;
+import com.amazonaws.services.sns.model.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -26,7 +29,9 @@ import java.util.List;
 import static eu.openg.aws.sns.internal.SNSExceptionBuilder.*;
 import static java.util.UUID.randomUUID;
 
-public class AmazonSNSFake extends AbstractAmazonSNS {
+public class AmazonSNSFake implements AmazonSNS {
+
+    private static final String NOT_IMPLEMENTED = "Extend AbstractAmazonSNS to provide an implementation";
 
     private final boolean authorized;
     private final String clientTokenId;
@@ -40,6 +45,231 @@ public class AmazonSNSFake extends AbstractAmazonSNS {
 
     public AmazonSNSFake(String clientTokenId) {
         this(clientTokenId, true);
+    }
+
+    @Override
+    public void setEndpoint(String endpoint) throws IllegalArgumentException {
+        throw new UnsupportedOperationException(NOT_IMPLEMENTED);
+    }
+
+    @Override
+    public void setRegion(Region region) throws IllegalArgumentException {
+        throw new UnsupportedOperationException(NOT_IMPLEMENTED);
+    }
+
+    @Override
+    public ConfirmSubscriptionResult confirmSubscription(ConfirmSubscriptionRequest confirmSubscriptionRequest) {
+        throw new UnsupportedOperationException(NOT_IMPLEMENTED);
+    }
+
+    @Override
+    public CreatePlatformApplicationResult createPlatformApplication(
+            CreatePlatformApplicationRequest createPlatformApplicationRequest
+    ) throws AmazonClientException {
+        throw new UnsupportedOperationException(NOT_IMPLEMENTED);
+    }
+
+    @Override
+    public GetTopicAttributesResult getTopicAttributes(GetTopicAttributesRequest getTopicAttributesRequest) {
+        throw new UnsupportedOperationException(NOT_IMPLEMENTED);
+    }
+
+    @Override
+    public SubscribeResult subscribe(SubscribeRequest subscribeRequest) throws AmazonClientException {
+        throw new UnsupportedOperationException(NOT_IMPLEMENTED);
+    }
+
+    @Override
+    public void deleteEndpoint(DeleteEndpointRequest deleteEndpointRequest) throws AmazonClientException {
+        throw new UnsupportedOperationException(NOT_IMPLEMENTED);
+    }
+
+    @Override
+    public void setTopicAttributes(SetTopicAttributesRequest setTopicAttributesRequest) throws AmazonClientException {
+        throw new UnsupportedOperationException(NOT_IMPLEMENTED);
+    }
+
+    @Override
+    public void deleteTopic(DeleteTopicRequest deleteTopicRequest) throws AmazonClientException {
+        throw new UnsupportedOperationException(NOT_IMPLEMENTED);
+    }
+
+    @Override
+    public void removePermission(RemovePermissionRequest removePermissionRequest) throws AmazonClientException {
+        throw new UnsupportedOperationException(NOT_IMPLEMENTED);
+    }
+
+    @Override
+    public GetEndpointAttributesResult getEndpointAttributes(
+            GetEndpointAttributesRequest getEndpointAttributesRequest
+    ) throws AmazonClientException {
+        throw new UnsupportedOperationException(NOT_IMPLEMENTED);
+    }
+
+    @Override
+    public GetPlatformApplicationAttributesResult getPlatformApplicationAttributes(
+            GetPlatformApplicationAttributesRequest getPlatformApplicationAttributesRequest
+    ) throws AmazonClientException {
+        throw new UnsupportedOperationException(NOT_IMPLEMENTED);
+    }
+
+    @Override
+    public ListSubscriptionsResult listSubscriptions(ListSubscriptionsRequest listSubscriptionsRequest) {
+        throw new UnsupportedOperationException(NOT_IMPLEMENTED);
+    }
+
+    @Override
+    public void setSubscriptionAttributes(SetSubscriptionAttributesRequest setSubscriptionAttributesRequest) {
+        throw new UnsupportedOperationException(NOT_IMPLEMENTED);
+    }
+
+    @Override
+    public void setPlatformApplicationAttributes(
+            SetPlatformApplicationAttributesRequest setPlatformApplicationAttributesRequest
+    ) throws AmazonClientException {
+        throw new UnsupportedOperationException(NOT_IMPLEMENTED);
+    }
+
+    @Override
+    public CreatePlatformEndpointResult createPlatformEndpoint(
+            CreatePlatformEndpointRequest createPlatformEndpointRequest
+    ) throws AmazonClientException {
+        throw new UnsupportedOperationException(NOT_IMPLEMENTED);
+    }
+
+    @Override
+    public void addPermission(AddPermissionRequest addPermissionRequest) throws AmazonClientException {
+        throw new UnsupportedOperationException(NOT_IMPLEMENTED);
+    }
+
+    @Override
+    public GetSubscriptionAttributesResult getSubscriptionAttributes(
+            GetSubscriptionAttributesRequest getSubscriptionAttributesRequest
+    ) throws AmazonClientException {
+        throw new UnsupportedOperationException(NOT_IMPLEMENTED);
+    }
+
+    @Override
+    public CreateTopicResult createTopic(CreateTopicRequest createTopicRequest) throws AmazonClientException {
+        throw new UnsupportedOperationException(NOT_IMPLEMENTED);
+    }
+
+    @Override
+    public ListTopicsResult listTopics(ListTopicsRequest listTopicsRequest) throws AmazonClientException {
+        throw new UnsupportedOperationException(NOT_IMPLEMENTED);
+    }
+
+    @Override
+    public ListEndpointsByPlatformApplicationResult listEndpointsByPlatformApplication(
+            ListEndpointsByPlatformApplicationRequest listEndpointsByPlatformApplicationRequest
+    ) throws AmazonClientException {
+        throw new UnsupportedOperationException(NOT_IMPLEMENTED);
+    }
+
+    @Override
+    public void deletePlatformApplication(DeletePlatformApplicationRequest deletePlatformApplicationRequest) {
+        throw new UnsupportedOperationException(NOT_IMPLEMENTED);
+    }
+
+    @Override
+    public ListPlatformApplicationsResult listPlatformApplications(
+            ListPlatformApplicationsRequest listPlatformApplicationsRequest
+    ) throws AmazonClientException {
+        throw new UnsupportedOperationException(NOT_IMPLEMENTED);
+    }
+
+    @Override
+    public void setEndpointAttributes(SetEndpointAttributesRequest setEndpointAttributesRequest) {
+        throw new UnsupportedOperationException(NOT_IMPLEMENTED);
+    }
+
+    @Override
+    public void unsubscribe(UnsubscribeRequest unsubscribeRequest) throws AmazonClientException {
+        throw new UnsupportedOperationException(NOT_IMPLEMENTED);
+    }
+
+    @Override
+    public ListSubscriptionsByTopicResult listSubscriptionsByTopic(
+            ListSubscriptionsByTopicRequest listSubscriptionsByTopicRequest
+    ) throws AmazonClientException {
+        throw new UnsupportedOperationException(NOT_IMPLEMENTED);
+    }
+
+    @Override
+    public PublishResult publish(PublishRequest publishRequest) throws AmazonClientException {
+        throw new UnsupportedOperationException(NOT_IMPLEMENTED);
+    }
+
+    @Override
+    public ListSubscriptionsResult listSubscriptions() throws AmazonClientException {
+        throw new UnsupportedOperationException(NOT_IMPLEMENTED);
+    }
+
+    @Override
+    public ListTopicsResult listTopics() throws AmazonClientException {
+        throw new UnsupportedOperationException(NOT_IMPLEMENTED);
+    }
+
+    @Override
+    public ListPlatformApplicationsResult listPlatformApplications() throws AmazonClientException {
+        throw new UnsupportedOperationException(NOT_IMPLEMENTED);
+    }
+
+    @Override
+    public ConfirmSubscriptionResult confirmSubscription(
+            String topicArn, String token, String authenticateOnUnsubscribe
+    ) throws AmazonClientException {
+        throw new UnsupportedOperationException(NOT_IMPLEMENTED);
+    }
+
+    @Override
+    public ConfirmSubscriptionResult confirmSubscription(String topicArn, String token) throws AmazonClientException {
+        throw new UnsupportedOperationException(NOT_IMPLEMENTED);
+    }
+
+    @Override
+    public GetTopicAttributesResult getTopicAttributes(String topicArn) throws AmazonClientException {
+        throw new UnsupportedOperationException(NOT_IMPLEMENTED);
+    }
+
+    @Override
+    public SubscribeResult subscribe(String topicArn, String protocol, String endpoint) throws AmazonClientException {
+        throw new UnsupportedOperationException(NOT_IMPLEMENTED);
+    }
+
+    @Override
+    public void setTopicAttributes(String topicArn, String attributeName, String attributeValue) {
+        throw new UnsupportedOperationException(NOT_IMPLEMENTED);
+    }
+
+    @Override
+    public void deleteTopic(String topicArn) throws AmazonClientException {
+        throw new UnsupportedOperationException(NOT_IMPLEMENTED);
+    }
+
+    @Override
+    public void removePermission(String topicArn, String label) throws AmazonClientException {
+        throw new UnsupportedOperationException(NOT_IMPLEMENTED);
+    }
+
+    @Override
+    public ListSubscriptionsResult listSubscriptions(String nextToken) throws AmazonClientException {
+        throw new UnsupportedOperationException(NOT_IMPLEMENTED);
+    }
+
+    @Override
+    public void setSubscriptionAttributes(String subscriptionArn, String attributeName, String attributeValue) {
+        throw new UnsupportedOperationException(NOT_IMPLEMENTED);
+    }
+
+    @Override
+    public void addPermission(String topicArn, String label, List<String> aWSAccountIds, List<String> actionNames) {
+        throw new UnsupportedOperationException(NOT_IMPLEMENTED);
+    }
+
+    @Override
+    public GetSubscriptionAttributesResult getSubscriptionAttributes(String subscriptionArn) {
+        throw new UnsupportedOperationException(NOT_IMPLEMENTED);
     }
 
     @Override
@@ -57,6 +287,26 @@ public class AmazonSNSFake extends AbstractAmazonSNS {
     }
 
     @Override
+    public ListTopicsResult listTopics(String nextToken) throws AmazonClientException {
+        throw new UnsupportedOperationException(NOT_IMPLEMENTED);
+    }
+
+    @Override
+    public void unsubscribe(String subscriptionArn) throws AmazonClientException {
+        throw new UnsupportedOperationException(NOT_IMPLEMENTED);
+    }
+
+    @Override
+    public ListSubscriptionsByTopicResult listSubscriptionsByTopic(String topicArn, String nextToken) {
+        throw new UnsupportedOperationException(NOT_IMPLEMENTED);
+    }
+
+    @Override
+    public ListSubscriptionsByTopicResult listSubscriptionsByTopic(String topicArn) throws AmazonClientException {
+        throw new UnsupportedOperationException(NOT_IMPLEMENTED);
+    }
+
+    @Override
     public PublishResult publish(String topicArn, String message) throws AmazonClientException {
         final SNSTopicArn arn = new SNSTopicArn(topicArn);
         if (!clientTokenId.equals(arn.getClientTokenId()))
@@ -64,5 +314,20 @@ public class AmazonSNSFake extends AbstractAmazonSNS {
         if (!topics.contains(arn.getTopic()))
             throw buildNotFoundException("Topic does not exist");
         return new PublishResult().withMessageId(randomUUID().toString());
+    }
+
+    @Override
+    public PublishResult publish(String topicArn, String message, String subject) throws AmazonClientException {
+        throw new UnsupportedOperationException(NOT_IMPLEMENTED);
+    }
+
+    @Override
+    public void shutdown() {
+        throw new UnsupportedOperationException(NOT_IMPLEMENTED);
+    }
+
+    @Override
+    public ResponseMetadata getCachedResponseMetadata(AmazonWebServiceRequest request) {
+        throw new UnsupportedOperationException(NOT_IMPLEMENTED);
     }
 }
